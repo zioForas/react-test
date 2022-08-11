ec71ade735b22b83f82913b078132eeae2de03ff
Anto <107312386+zioForas@users.noreply.github.com>
2022-08-11 15:05:37 +0100

    more stuff added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+ import React from "react";
+
+ function Footer() {
+   return (
+     <footer>
+       <h3>im the coolest footer in the jungle</h3>
+     </footer>
+   );
+ }
+
+ export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+ function Navbar() {
+    return ( 
+     <nav>
+         <h4 className="navigation">Home About coolest Navbar World </h4>
+    </nav>
+ )
+ }
+
+ export default Navbar
